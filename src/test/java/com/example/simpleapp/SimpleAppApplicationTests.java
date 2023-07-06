78d8bc715448bfa37ee88264ebb390dc1cab7b49
frovera <francesco@urbi.co>
2023-07-06 10:56:47 +0200

    test

--- src/test/java/com/example/simpleapp/SimpleAppApplicationTests.java
+++ src/test/java/com/example/simpleapp/SimpleAppApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.simpleapp;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class SimpleAppApplicationTests {
 
     @Test
     void contextLoads() {
+        Assert.isTrue(Math.random() > 0.5);
     }
 
 }
