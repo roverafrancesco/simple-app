618d395f036da071065ef9afeb72f46453854087
frovera <francesco@urbi.co>
2023-07-06 17:54:11 +0200

    a

--- src/test/java/com/example/simpleapp/SimpleAppApplicationTests.java
+++ src/test/java/com/example/simpleapp/SimpleAppApplicationTests.java
@@ -16,7 +16,7 @@ class SimpleAppApplicationTests {
 
     @Test
     void test1() {
-        boolean exp = Math.random() > 0;
+        boolean exp = Math.random() > 0.5;
         assertTrue("Error", exp);
     }
 
